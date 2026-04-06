2306b825e13d0106cc5654b621dc7756e9b61d47
Youngguan Yu <157783368+VBKOROA@users.noreply.github.com>
2026-04-06 20:16:29 +0900

    [level 1] Title: 약수의 합, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                int pair = n / i;
+
+                answer += i;
+
+                if (pair != i)
+                {
+                    answer += n / i;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
