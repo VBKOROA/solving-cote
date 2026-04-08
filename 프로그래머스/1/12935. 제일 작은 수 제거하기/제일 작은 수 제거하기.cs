e437101d2d82a7ff31f97b522645c43d3a26142a
Youngguan Yu <157783368+VBKOROA@users.noreply.github.com>
2026-04-08 23:32:48 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.81 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cs
@@ -0,0 +1,14 @@
+using System.Linq;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        if (arr.Length == 1)
+        {
+            return new int[] {-1};
+        }
+
+        int min = arr.Min();
+        int[] answer = arr.Where(num => num != min).ToArray();
+        return answer;
+    }
+}
